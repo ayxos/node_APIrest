f046f39a748ac3884bdd18b949553c43f0174c90
cybermarkus1 <cybermarkus1@gmail.com>
2014-02-06 20:31:15 +0100

    adding xpressio

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -47,3 +47,46 @@ $(document).ready(function(){
     });
 
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+//     document.getElementById("update").addEventListener("click", function(e) {
+//         console.log("you clicked " + e.target);
+//         e.preventDefault();
+
+//         var URL = ("/api/entries/" + id);
+
+//         var clave = document.getElementById('keys').value;
+//         var valor = document.getElementById('value').value;
+
+//         var arr = {};
+//         arr[clave] = valor;
+//         console.log(arr);
+//         var data = JSON.stringify(arr);
+
+//         var id = this.getAttribute('data-id');
+//         var URL = ("/api/entries/" + id);
+
+//         var request = new XMLHttpRequest();
+//         request.open('PUT', URL, true);
+
+//         request.onload = function() {
+//           if (request.status >= 200 && request.status < 400){
+//             console.log("Updating success", request);
+//             // location.reload();
+//             // resp = request.responseText;
+//           } else {
+//             // We reached our target server, but it returned an error
+//             console.log("Update fail");
+//             console.log(status);
+//             console.log(jqXHR);
+//           }
+//         }
+
+//         request.onerror = function() {
+//           // There was a connection error of some sort
+//           console.log('error on req');
+//         }
+//         request.send(data);
+//     });
+// });
